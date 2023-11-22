a64cd55bbb72f5f9b45669ce78e2f29def530dbc
SiddhantSilwal <silwalsiddhant@gmail.com>
2023-11-22 10:24:35 +0545

    Basic files added

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let stars = document.getElementById('star')
+let front_planets = document.getElementById('front_planets')
+let back_planets = document.getElementById('back_planets')
+let text = document.getElementById('text')
+let btn = document.getElementById('btn')
+
+window.addEventListener('scroll',function(){
+    let value = window.scrollY;
+    stars.style.top = -value * 0.5 + 'px';
+    front_planets.style.top = value * 1.02 + 'px';
+    back_planets.style.top = -value * 0.09 + 'px';
+})
